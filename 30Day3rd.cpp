b55180097df6d96b76a8aaa3c760a785a737082e
Ujjwal  Jha <72433046+Jhaujjwal4196@users.noreply.github.com>
2021-01-07 10:05:48 +0545

    3rd problem done

---
+++ 30Day3rd.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+int main()
+{
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    if(n%2!=0)
+    cout<<"Weird";
+    else if(n>=2&&n<=5)
+    cout<<"Not Weird";
+    else if(n>5&&n<=20)
+    cout<<"Weird";
+    else {
+    cout<<"Not Weird";
+    }
+
+    return 0;
+}
